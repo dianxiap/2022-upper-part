34d1f7d50771ce09a59f99ca6f20121b8f8c3988
your-highness-p <1207864396@qq.com>
2022-11-27 23:22:29 +0800

    无

---
+++ Project22/Project22/源.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void test()
+{
+	printf("hehe");
+}
+
+int main()
+{
+
+	Test();
+}
---
+++ Project23/Project23/源.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10] = { 0 };
+    int i = 0;
+    for (i = 0; i <= 12; i++) {
+        arr[i] = 0;
+        printf("hehe\n");
+    }
+
+    return 0;
+}
