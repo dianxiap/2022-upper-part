42f4334091f7b883f049540248b20946a2dce5e5
your-highness-p <1207864396@qq.com>
2022-12-12 23:06:39 +0800

    0

---
+++ Project32/Project32/源.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+const int N = 1000010;
+
+int a[N], q[N];
+
+int n, k;
+
+int main()
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)cin >> a[i];
+    int hh = 0, tt = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (hh <= tt && i - k + 1 > q[hh])hh++;
+        while (hh <= tt && a[q[tt]] > a[i])tt--;
+        q[++tt] = i;
+        if (i >= k - 1)cout << a[q[hh]] << ' ';
+    }
+    cout << endl;
+    hh = 0, tt = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (hh <= tt && i - k + 1 > q[hh])hh++;
+        while (hh <= tt && a[q[tt]] < a[i])tt--;
+        q[++tt] = i;
+        if (i >= k - 1)cout << a[q[hh]] << ' ';
+    }
+}
+
+
